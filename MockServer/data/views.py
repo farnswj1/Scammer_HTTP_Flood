4c6668db5190ffd1db54a1a741cbbd09274b5035
Justin Farnsworth <farnswj1@tcnj.edu>
2021-04-03 13:06:58 -0400

    added mock server to test script

---
+++ MockServer/data/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from .serializers import DataSerializer
+from .models import Data
+
+# Create your views here.
+class ListCreateData(ListCreateAPIView):
+    queryset = Data.objects.all()
+    serializer_class = DataSerializer
